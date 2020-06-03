ea825c18916573f98bb84f484757467dab7db3b6
MOHINI TRIPATHI <trishmohi01@gmail.com>
2020-06-03 22:00:01 +0530

    prepend added

---
+++ src/learn_clojure/linked_list.clj
@@ -0,0 +1,60 @@
+(ns learn-clojure.linked-list
+  (:refer-clojure :exclude [next]))
+
+(defprotocol Node
+  "Node for the list"
+  (data [node] "Returns data field of the node")
+  (next [node] "Returns next field of the node"))
+
+(defrecord NodeImpl [_data _next]
+  Node
+  (data [_] _data)
+  (next [_] _next))
+
+(defprotocol List
+  "Returns Linked List"
+  (head [this] "Returns first node of the list")
+  (tail [this] "Returns last node of the list")
+  (prepend [list value] "Returns the new list after adding a node with value at the head of the list")
+  (append [list value] "Returns the new list after adding a node with value at the tail of the list"))
+
+(defn node
+  ([]
+   (->NodeImpl nil nil))
+  ([data]
+   (->NodeImpl data nil))
+  ([data next]
+   (->NodeImpl data next)))
+
+(defrecord ListImpl [_head _tail]
+  List
+  (head [_] _head)
+  (tail [_] _tail)
+  (prepend [value list]
+    (let [new-node (node value (head list))]
+      (->ListImpl new-node (tail list))))
+  (append [value list]
+    (let [new-node (node value nil)]
+      (->ListImpl (head list) new-node))))
+
+
+
+(defn _list 
+  ([] 
+  (->ListImpl nil nil)) 
+  ([head tail]
+   (->ListImpl head tail)))
+
+
+
+(defn _prepend [value list]
+  (let [new-node (node value (head list))]
+    (->ListImpl new-node (tail list))))
+
+(defn _append [value list]
+(let [new-node (node value nil)]
+  (->ListImpl (head list) new-node)))
+
+(_prepend 1 (_list 2 3))
+(_append 3 (_list 1 2))
+
--- test/learn_clojure/linked_list_test.clj
+++ test/learn_clojure/linked_list_test.clj
@@ -1,54 +1,34 @@
 (ns learn-clojure.linked-list-test
   (:require 
-   [clojure.test :refer [deftest is are testing]]
+   [clojure.test :refer [deftest are testing]]
    [learn-clojure.linked-list :as list]))
 
-
 (deftest test-linked-list-node
-  (let [node-data-1 "foo"
+  (let [node-data-1 1
         node-1 (list/node node-data-1)
-        node-data-2 "bar"
-        node-2 (list/node node-data-2 node-1)]
-    (testing "create node from data"
-      (are [expected got] (= expected got)
-        node-data-1 (list/data node-1)
-        nil (list/next node-1)))
+        node-data-2 2
+        node-2 (list/node node-data-2 node-1)
+        node-data-3 3
+        node-3 (list/node node-data-3 node-2)]
+  (testing "create node from data"
+    (are [expected got] (= expected got)
+      node-data-1 (list/data node-1)
+      nil (list/next node-1)
+      node-data-2 (list/data node-2)
+      node-1 (list/next node-2)
+      node-data-3 (list/data node-3)
+      node-2 (list/next node-3)))
     (testing "link nodes together"
       (are [expected got] (= expected got)
         node-data-2 (list/data node-2)
         node-1 (list/next node-2)
-        nil (-> node-2
-                (list/next)
-                (list/next))))))
+        nil (list/next node-1)))))
 
 (deftest test-linked-list
-  (let [list-1 1]
-    (testing "create an empty list - linked-list"
-      (is (= {} (list/linked-list))))
-    (testing "add node to the last list - append"
-      (are [expected got] (= expected got)
-        1 1))
-    (testing "add node in start of th elist - prepend"
-      (are [expected got] (= expected got)
-        1 1))
-    (testing "delete node in the list by value  - delete"
-      (are [expected got] (= expected got)
-        1 1))
-    (testing "delete node from the head - delete-head"
-      (are [expected got] (= expected got)
-        1 1))
-    (testing "delete node from the tail - delete-tail"
-      (are [expected got] (= expected got)
-        1 1))
-    (testing "find node by value - find"
-      (are [expected got] (= expected got)
-        1 1))
-    
-    (testing "create list from a vector - vector->list"
-      (are [expected got] (= expected got)
-        1 1))
-    
-    (testing "return vector from the list - list->vector"
-      (are [expected got] (= expected got)
-        1 1))
-    ))
+  (let [list-1 (list/empty-linked-list)
+        head (list/head list-1)
+        tail (list/tail list-1)]
+    (testing "empty-linked-list"
+    (are [expected got] (= expected got)
+       nil head
+       nil tail))))
