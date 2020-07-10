bdfb2793eef2c00eb9e31a99a2a385140e85d333
MOHINI TRIPATHI <trishmohi@gmail.com>
2020-07-10 14:10:33 +0530

    connect method for dca

--- src/learn_clojure/Algorithms/dc.clj
+++ src/learn_clojure/Algorithms/dc.clj
@@ -11,18 +11,38 @@
     (concat a-set (set/difference (set (take n (range)))
                                   a-set))))
 
-(defprotocol dca
-  "Protocol for Dynamic Connectivity Algorithm"
+;; (defprotocol dca
+;;   "Protocol for Dynamic Connectivity Algorithm"
   
   
-  (union
-   [_]
-   "Returns a set of inputs")
+;;   (connect
+;;    [& args]
+;;    "Returns a set of inputs")
   
-  (connnected?
-   [_]
-   "Returns true if all the number is in a set"))
+;;   (connnected?
+;;    [& args]
+;;    "Returns true if all the number is in a set"))
 
+;; (defrecord dcaImpl [& args]
+;;   dca
+  
+;;   (connect
+;;    [& args])
+  
+;;   (connnected?
+;;    [& args]))
 
+(defn connect
+  [& args]
+  (let [obj (set (random-objects 10))
+        new-set (set args)
+        diff (set/difference obj new-set)]
+    (if (= true (set/intersection obj new-set))
+      (conj diff new-set)
+      (print "404"))))
 
-(random-objects 10)
+;; (defn connnected?
+;;   [& args]
+;;   (let [new-set (set args)]))
+
+(connect 1 2 3)
