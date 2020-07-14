b35352ea7813c21f40be85941017e2698181fb92
MOHINI TRIPATHI <trishmohi@gmail.com>
2020-07-14 11:25:03 +0530

    dc addition

--- src/learn_clojure/Algorithms/dc.clj
+++ src/learn_clojure/Algorithms/dc.clj
@@ -2,51 +2,34 @@
 ;; **************************************************
 
 
-(ns learn-clojure.Algorithms.dc
-  (:require 
-   [clojure.set :as set]))
-
-;; (defn random-objects [n]
-;;   (let [a-set (set (take n (repeatedly #(rand-int n))))]
-;;     (concat a-set (set/difference (set (take n (range)))
-;;                                   a-set))))
-
-;; (defprotocol dca
-;;   "Protocol for Dynamic Connectivity Algorithm"
-
-(def n1 '(1 2 3 4 5 6 7 8 9))
-  {1 2 3 4....} 
-{1 2 3}
-{4 5 6 7 {1 2 3} 1 2 3}
-  
-(defn connect
-  [obj & args]
-  (let [set-obj (set obj)
-        set-args (set args)
-        diff (set/difference set-obj set-args)]
-    (if (= set-args (set/intersection set-obj set-args))
-      (into '() (conj diff set-args))
-      nil)))
-  
-;;   (connected?
-;;    [& args]
-;;    "Returns true if all the number is in a set"))
-
-;; (defrecord dcaImpl [& args]
-;;   dca
-  
-(connect n1 3 5 4)
+(ns learn-clojure.Algorithms.dc)
 
 
-;; (defn connect
-;;   [& args]
-;;   (let [obj (set (random-objects 10))
-;;         new-set (set args)
-;;         diff (set/difference obj new-set)]
-;;     (if (= new-set (set/intersection obj new-set))
-;;       (conj diff new-set)
-;;       (print "object not found")
-;;       )))
+(defprotocol DC
+  (connect [this source target] "Connects source and target")
+  (connected? [this source target] "Returns true if source and target are connected"))
+
+(defrecord DCImpl [components]
+  DC
+  (connect 
+   [_ source target])
+
+  (connected?
+    [_ source]
+    (loop [curr (first components)
+           res (rest components)]
+      (if (nil? curr)
+        false
+        (if (and (contains? curr source))
+          true
+          (recur (first res) (rest res)))))))
 
 
-(connect n1 1 2 3)
+(defn connected-components
+  [& args]
+  (let [components (map (fn [item] #{item}) args)]
+    (->DCImpl components)))
+
+(def c1 (connected-components 1 2 3 4 5))
+
+(connected? c1 1 2)
